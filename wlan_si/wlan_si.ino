56f0d6b3522813cb80d2ff7de60cb732802a95d9
Dobrica Pavlinusic <dpavlin@rot13.org>
2015-04-23 15:21:06 +0200

    read MPL115A2
    based on hardware from wlan.si:
    https://dev.wlan-si.net/wiki/Telemetry/sensgw

---
+++ wlan_si/wlan_si.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <Adafruit_MPL115A2.h>
+
+Adafruit_MPL115A2 mpl115a2;
+
+int led = 5; // LED on d5
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  
+  Serial.println("Getting barometric pressure ...");
+  mpl115a2.begin();
+  
+  pinMode(led, OUTPUT);
+}
+
+void loop(void) 
+{
+  digitalWrite(led, HIGH);
+
+  float pressureKPA = 0, temperatureC = 0;    
+
+  mpl115a2.getPT(&pressureKPA,&temperatureC);
+  Serial.print("Pressure: "); Serial.print(pressureKPA, 4); Serial.println(" kPa");
+  Serial.print("Temp: "); Serial.print(temperatureC, 1); Serial.println(" *C");
+
+/*  
+  pressureKPA = mpl115a2.getPressure();  
+  Serial.print("Pressure (kPa): "); Serial.print(pressureKPA, 4); Serial.println(" kPa");
+
+  temperatureC = mpl115a2.getTemperature();  
+  Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
+*/
+
+  digitalWrite(led, LOW);
+  
+  delay(1000);
+}
