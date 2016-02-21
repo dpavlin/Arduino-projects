1fd5755a49d305837574bbf366a0f04a61d399c5
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-02-21 12:56:24 +0100

    /dev/serial/by-id/

--- Desk_LED_dimmer/fade.sh
+++ Desk_LED_dimmer/fade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-microcom -p /dev/ttyUSB1 | \
-while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | \
+microcom -p /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9UHTFJF-if00-port0 | \
+while IFS= read -r line; do printf '%s %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | \
 tee -a /tmp/fade.log
 
 #gawk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' | \
