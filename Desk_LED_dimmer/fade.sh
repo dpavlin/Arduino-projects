054bed9000c6ff9b0f4873e1d8694975ca842166
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-05-01 12:10:58 +0200

    socat serial example

--- Desk_LED_dimmer/fade.sh
+++ Desk_LED_dimmer/fade.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+#socat - /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9UHTFJF-if00-port0 | \
 microcom -p /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9UHTFJF-if00-port0 | \
 while IFS= read -r line; do printf '%s %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done | \
 tee -a /tmp/fade.log
