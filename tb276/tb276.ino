1021f87276e8c9ab2db19940ac8c938128c58bec
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-07-08 21:49:59 +0200

    select led to blink with keys

--- tb276/tb276.ino
+++ tb276/tb276.ino
@@ -1,33 +1,32 @@
 /*
- Double Fade
 
+scrolling led on buttons
 
- This example shows how to fade an LEDs on pin 9 and 10
- using the analogWrite() function.
-
- This example code is in the public domain.
  */
 
-int led1 = 9;           // the pin that the LED is attached to
-int led2 = 10;
-int led3 = 11;
-int led4 = 12;
+extern "C" {
+#include <dev/io.h> // needed for INB
+}
+
+#define NR_LEDS 8
+int leds[NR_LEDS] = {8,9,10,11,12,13,14,15};           // the pin that the LED is attached to
+	// I don't really understand where this numbers are comming from yet :-(
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 int count = 0;
+int pos = 0;
+int old_key = -1;
+int key_micros = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+  for(int i=0; i<NR_LEDS; i++) {
+	pinMode(leds[i], OUTPUT);
+	digitalWrite(leds[i], LOW);
+  }
+
   analogWriteResolution(12);
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, LOW);
 
   Serial.begin(115200);
   Serial.println("setup");
@@ -41,35 +40,46 @@ void loop() {
   mode &= 16383;
   if((mode & 8192) == 0)
   {
-    analogWrite(led1, brightness);
-    analogWrite(led2, 4095-brightness);
-    digitalWrite(led3, (mode & 256) == 0 ? LOW  : HIGH);
-    digitalWrite(led4, (mode & 256) == 0 ? HIGH : LOW);
+    analogWrite(leds[pos], brightness);
   }
   else
   {
-    digitalWrite(led1, (mode & 256) == 0 ? LOW  : HIGH);
-    digitalWrite(led2, (mode & 256) == 0 ? HIGH : LOW);
-    analogWrite(led3, brightness);
-    analogWrite(led4, 4095-brightness);
+    digitalWrite(leds[pos], (mode & 256) == 0 ? LOW  : HIGH);
   }
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
-  if (brightness % 100 == 0 ) {
-    Serial.print( int(brightness / 100) );
-    Serial.print(" ");
-  }
 
   // reverse the direction of the fading at the ends of the fade:
   if (brightness == 0 || brightness == 4095) {
     fadeAmount = -fadeAmount ;
-    Serial.println();
     Serial.print(micros());
     Serial.print(" ");
-    Serial.print(count++);
-    Serial.print(" ");
+    Serial.println(count++);
   }
+
+
+  int old_pos = pos;
+  int key;
+  INB(key, IO_PUSHBTN);
+  if ( key != old_key && micros() - key_micros > 300) {
+	old_key = key;
+	if (key & BTN_RIGHT && pos >= 0) pos--;
+	if (key & BTN_LEFT  && pos < (NR_LEDS-1)) pos++;
+	if ( pos != old_pos ) {
+		Serial.print("key ");
+		Serial.print(key);
+		Serial.print(" pos=");
+		Serial.print(pos);
+		Serial.print(" micros diff=");
+		Serial.println(micros()-key_micros);
+		digitalWrite(leds[old_pos], LOW);
+		digitalWrite(leds[pos], HIGH);
+	}
+	key_micros = micros();
+  }
+
+
   // wait for 30 milliseconds to see the dimming effect
   delay(1);
 }
