0a6ce334eb7b381783565f83f04cc4180ed226cf
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-11-23 12:18:10 +0100

    added runningAverage for pressure

--- wlan_si/wlan_si.ino
+++ wlan_si/wlan_si.ino
@@ -9,6 +9,24 @@ int pir = 6; // PIR on d6
 // hardware based on https://dev.wlan-si.net/wiki/Telemetry/sensgw
 // original software https://github.com/SloMusti/sensgw
 
+float runningAverage(float M) {
+  #define LM_SIZE 100
+  static float LM[LM_SIZE];      // LastMeasurements
+  static byte index = 0;
+  static float sum = 0;
+  static byte count = 0;
+
+  // keep sum updated to improve speed.
+  sum -= LM[index];
+  LM[index] = M;
+  sum += LM[index];
+  index++;
+  index = index % LM_SIZE;
+  if (count < LM_SIZE) count++;
+
+  return sum / count;
+}
+
 void setup(void) 
 {
   Serial.begin(9600);
@@ -21,6 +39,8 @@ void setup(void)
   pinMode(pir, INPUT);
 }
 
+int count = 0;
+
 void loop(void) 
 {
   digitalWrite(led, HIGH);
@@ -28,7 +48,9 @@ void loop(void)
   float pressureKPA = 0, temperatureC = 0;    
 
   mpl115a2.getPT(&pressureKPA,&temperatureC);
-  Serial.print("Pressure="); Serial.print(pressureKPA, 4); Serial.print(" kPa ");
+
+  if ( count++ % 10 == 0 ) {
+  Serial.print("Pressure="); Serial.print(runningAverage(pressureKPA), 4); Serial.print(" kPa ");
   Serial.print("Temp="); Serial.print(temperatureC, 1); Serial.print(" C");
 
   Serial.print(" PIR="); Serial.print( digitalRead(pir) );
@@ -37,7 +59,11 @@ void loop(void)
 
   Serial.println();
 
+  } else {
+	runningAverage(pressureKPA);
+  }
+
   digitalWrite(led, LOW);
   
-  delay(1000);
+  delay(100);
 }
